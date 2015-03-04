2ab944584ed69b85a88f54ad2ec98deedc5a8e2c
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-03-04 14:27:29 +0100

    fix Logo in the header

--- app/scripts/components/header.jsx
+++ app/scripts/components/header.jsx
@@ -3,20 +3,18 @@ import React from 'react';
 var Header = React.createClass({
 
   render() {
-    return (
-      <header className="clearfix">
-          <div class="navbar navbar-default navbar-fixed-top yamm" role="navigation" id="navbar">
-              <div class="container">
-                  <div class="navbar-header">
-                      <a class="navbar-brand home" href="/">
-                          <img src="images/logo.png" height="51" width="160" alt="Traze.es" class="hidden-xs hidden-sm" />
-                          <img src="images/logo-small.png" height="51" width="50" alt="Traze.es" class="visible-xs visible-sm" />
-                          <span class="sr-only">Traze</span>
-                      </a>
-                  </div>
-              </div>
-          </div>
-      </header>
+    return (      
+        <div className="navbar navbar-default navbar-fixed-top yamm" role="navigation" id="navbar">
+            <div className="container">
+                <div className="navbar-header">
+                    <a className="navbar-brand home" href="/">
+                        <img src="images/logo.png" height="51" width="160" alt="Traze.es" className="hidden-xs hidden-sm" />
+                        <img src="images/logo-small.png" height="51" width="50" alt="Traze.es" className="visible-xs visible-sm" />
+                        <span className="sr-only">Traze</span>
+                    </a>
+                </div>
+            </div>
+        </div>      
     );
   }
 
