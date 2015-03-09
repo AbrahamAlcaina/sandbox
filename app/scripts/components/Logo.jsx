85b87ab4b4b904d855dcf10c99a0c436a5c4c31d
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-03-09 16:02:08 +0100

    Add logo

---
+++ app/scripts/components/Logo.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+import {addons} from 'react/addons';
+
+var Logo = React.createClass({
+	mixins: [addons.PureRenderMixin],
+	render(){
+		return (
+                <a className="navbar-brand home" href="/">
+                    <img src="images/logo.png" height="51" width="160" alt="Traze.es" className="hidden-xs hidden-sm" />
+                    <img src="images/logo-small.png" height="51" width="50" alt="Traze.es" className="visible-xs visible-sm" />
+                    <span className="sr-only">Traze</span>
+                </a>
+			);
+	}
+});
+
+module.exports = Logo;
